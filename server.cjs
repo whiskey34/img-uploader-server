62c9a8ee2ccdfcf1ab2a9e6f5f7f42fde2efb226
whiskey34 <black.delfino28@gmail.com>
2022-10-10 22:39:01 +0700

    make it better

--- server.cjs
+++ server.cjs
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static('/dist'));
 app.use(helmet());
 app.use(cors());
-app.use(serveStatic(__dirname, '/dist'));
+app.use(serveStatic(__dirname + '/dist'));
 
 mongoose
   .connect(process.env.CONN_URL, {
